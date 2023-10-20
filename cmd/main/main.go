bcc1f9934652dfb09ba91bad2195428e7c81c30d
Naman Pancholi <mahendra.naman07@gmail.com>
2023-10-20 13:22:56 +0530

    stuck with mysql connection

--- cmd/main/main.go
+++ cmd/main/main.go
@@ -1,1 +1,16 @@
 package main
+
+import (
+	"GO_with_MySQL/pkg/routes"
+	"github.com/gorilla/mux"
+	"log"
+	"net/http"
+)
+
+func main() {
+	r := mux.NewRouter()
+	routes.RegisterBookStoreRoutes(r)
+	http.Handle("/", r)
+	log.Fatal(http.ListenAndServe("localhost:8080", r))
+
+}
